b79e7e6dd8f6aebabb9617667c26ede61690b1f0
Frida Lannerström <frida.lannerstrom@tqnordic.com>
2026-01-25 14:56:57 +0100

    feat: settings for logging

--- config/settings.py
+++ config/settings.py
@@ -187,3 +187,22 @@ else:
     
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DEFAULT_FROM_EMAIL = "Talena <no-reply@talena.se>"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
