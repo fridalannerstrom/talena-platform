0f65db8b56cb3ee2642ef3e3b097decf3c987582
Frida Lannerström <frida.lannerstrom@tqnordic.com>
2026-02-04 20:32:06 +0100

    feat: login page styling

---
+++ apps/core/forms.py
@@ -0,0 +1,15 @@
+# forms.py (exempel)
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "class": "input",
+        "placeholder": "example-user",
+        "autocomplete": "username",
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        "class": "input",
+        "placeholder": "••••••••••••••",
+        "autocomplete": "current-password",
+    }))
