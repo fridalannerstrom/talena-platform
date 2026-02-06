b88923f0ccde2196f173af78aab3d1b83d203a63
Frida Lannerström <frida.lannerstrom@tqnordic.com>
2026-02-06 10:59:57 +0100

    feat: candidate sheet touch up

--- apps/emails/forms.py
+++ apps/emails/forms.py
@@ -8,3 +8,16 @@ class EmailTemplateForm(forms.ModelForm):
         widgets = {
             "body": forms.Textarea(attrs={"rows": 10}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            widget = field.widget
+
+            if name == "language":
+                widget.attrs.setdefault("class", "form-select")
+            elif name == "is_active":
+                widget.attrs.setdefault("class", "form-check-input")
+            else:
+                widget.attrs.setdefault("class", "form-control")
