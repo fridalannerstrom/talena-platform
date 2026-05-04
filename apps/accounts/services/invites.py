3fdb9519da08e16c3a091ab55fbb4540b0d9606e
Frida Lannerström <frida.lannerstrom@tqnordic.com>
2026-05-04 16:12:40 +0200

    feat: email invite design

--- apps/accounts/services/invites.py
+++ apps/accounts/services/invites.py
@@ -7,18 +7,23 @@ from django.utils.encoding import force_bytes
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.templatetags.static import static
 
 
 def send_invite_email(request, user, invite_link, company=None):
-    subject = "Du är inbjuden till Talena"
+    subject = "Välkommen till Talena"
+
+    logo_url = request.build_absolute_uri(static("images/talena-logo-email.png"))
 
     context = {
         "user": user,
         "company": company,
+        "company_name": company.name if company else "din organisation",
         "invite_link": invite_link,
+        "invited_by": request.user.get_full_name() or request.user.email,
+        "logo_url": logo_url,
     }
 
-    # Om du vill köra template:
     text_body = render_to_string("emails/invite.txt", context)
     html_body = render_to_string("emails/invite.html", context)
 
@@ -28,5 +33,6 @@ def send_invite_email(request, user, invite_link, company=None):
         from_email=getattr(settings, "DEFAULT_FROM_EMAIL", None),
         to=[user.email],
     )
+
     msg.attach_alternative(html_body, "text/html")
     msg.send()
